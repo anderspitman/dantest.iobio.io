13765a6e7ee18ccd518e30ab8fef0d094f6f99d7
Anders Pitman <tapitman11@gmail.com>
2020-01-31 17:25:51 -0700

    Basic read counting is working

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-console.log("Hi there");
+const uppie = new Uppie();
+uppie(document.querySelector('#file-input'), async (event, formData, files) => {
+
+  for (const entry of formData.entries()) {
+
+    const file = entry[1];
+
+    console.log(file);
+
+    const inflate = new pako.Inflate({ level: 3});
+
+    const decoder = new TextDecoder('utf-8');
+
+    let numReads = 0;
+    let totalLines = 0;
+    let readLengthSum = 0;
+    let remainder = '';
+
+    // process data, splitting on newlines
+    inflate.onData = (data) => {
+      let chunk = decoder.decode(data);
+
+      if (remainder.length > 0) {
+        chunk = remainder + chunk;
+        remainder = '';
+      }
+
+      let index = 0;
+      while (true) {
+        index = chunk.indexOf('\n');
+
+        if (index < 0) {
+          break;
+        }
+
+        // don't include the newline
+        const line = chunk.slice(0, index);
+        chunk = chunk.slice(index + 1);
+
+        const isSeqLine = totalLines % 4 === 1;
+        totalLines += 1;
+
+        if (isSeqLine) {
+          readLengthSum += line.length;
+          numReads += 1;
+        }
+      }
+
+      if (chunk.length > 0) {
+        remainder = chunk;
+      }
+    };
+
+    inflate.onEnd = (status) => {
+      console.log(readLengthSum, numReads, readLengthSum/numReads);
+    };
+
+    const chunkSize = 1024*1024;
+
+    function readChunk(file, offset) {
+
+      const fileReader = new FileReader();
+
+      fileReader.onload = (event) => {
+        const buf = new Uint8Array(event.target.result);
+        if (offset + chunkSize > file.size) {
+          const success = inflate.push(buf, true);
+          console.log("done", success);
+        }
+        else {
+          const success = inflate.push(buf, false);
+          readChunk(file, offset + chunkSize);
+        }
+      };
+
+      fileReader.readAsArrayBuffer(file.slice(offset, offset + chunkSize));
+    }
+
+    readChunk(file, 0);
+
+  }
+});
